600643902b1a8456f3009bbd060bf6e561a3ff4d
coolcode <coolcode@live.com>
2013-02-26 14:42:22 +0800

    延迟加载lazyload.js 瀑布流demo

---
+++ waterfall/Scripts/all.js
@@ -0,0 +1,132 @@
+﻿function initLayout() {
+    var window_width = $(window).width();
+    wrapper_width = Math.floor(window_width / 236) * 236;
+    if (window_width > 960) {
+        $('.wrapper').css("width", wrapper_width + "px");
+    } else {
+        $('.wrapper').css("width", "944px");
+    }
+}
+
+function resort() {
+    $('#masonry').masonry({
+        itemSelector: '.item',
+        isResizable: false
+    });
+}
+
+$(function () {
+    initLayout();
+    resort();
+});
+
+$(window).resize(function () {
+    initLayout();
+    resort();
+});
+
+var __lastIndex__ = "!@#";
+$(window).scroll(function () {
+    var o = $('#masonry');
+
+    // 并不是所有页面都要执行加载操作。
+    // 你也可以选择别的识别条件。
+    if (o != null && o.length != 0) {
+
+        //获取网页的完整高度(fix)
+        var hght = window.scrollHeight;
+
+        var srollPos = $(window).scrollTop();    //滚动条距顶部距离(页面超出窗口的高度)
+        var dbHiht = $("body").height();          //页面(约等于窗体)高度/单位px
+        var mainHiht = o.height();               //主体元素高度/单位px
+
+        if (($(document).height() - $(window).height() - srollPos) > 50) {
+            return;
+        }
+
+        //避免重复提交
+        if (__lastIndex__ == o.attr("index")) {
+            return;
+        }
+
+        __lastIndex__ = o.attr("index");
+        show();
+
+        return;
+
+    }
+});
+
+function show() {
+    var target = $('#masonry');
+
+    if (!target) { return false; }
+
+    if (target.attr("nomore")==1) {
+        return false;
+    }
+
+    var data = {};
+    data.next = target.attr("index");
+
+
+    var currentHref = window.location;
+
+    // 有的时候可能还要从url中读取参数，我使用正则式
+    /*
+    var tcaStr = new RegExp("(^|)tca=([^\&]*)(\&|$)", "gi").exec(currentHref);
+    if (tcaStr) data.tca = tcaStr[2];
+
+    var scaStr = new RegExp("(^|)sca=([^\&]*)(\&|$)", "gi").exec(currentHref);
+    if (scaStr) data.sca = scaStr[2];
+
+    var tagStr = new RegExp("(^|)tag=([^\&]*)(\&|$)", "gi").exec(currentHref);
+    if (tagStr) data.tag = tagStr[2];
+    */
+    // ajax请求数据
+    jQuery.ajax({
+        type: "POST",
+        url: "/Archive/Lazyload",
+        data: data,
+        dataType: "json",
+        cache:false,
+        beforeSend: function (XMLHttpRequest) {
+            $("#loading").css('display', '');
+        },
+        success: function (r) {
+        var data = r.Items;
+        var html="";
+            for (var i = 0, length = data.length; i < length; i++) {
+                var item = data[i];
+                html += "<div class='item'>" +
+                        "<h2 class='title'>"+
+                           "<a href='/p/"+ item.Id + "' target='_blank'>" + item.Title+ "</a>" + 
+                        "</h2>"+
+                        "<div class='comments'>"+
+                            item.Summary+
+                        "</div>" +
+                        "<div class='chat'>"+
+                            "<div class='chatbox'>" +
+                            "<img src='/Image/AppIcon/"+ item.IconPath+"' width='50px' height='50px' alt='' />"+
+                            "<b>"+item.Author + "</b><p>发布于"+ item.PublishTimeText + "</p>"+
+                            "</div>"+
+                        "</div>"+
+                    "</div>";
+            }
+
+            
+            var box = $(html);
+            target.append(box).masonry('appended',box,true);
+
+            target.attr('index', r.LastIndex);
+            target.attr('nomore', r.Count==0?1:0);
+
+            $("#loading").css('display', 'none');
+        },
+        error: function () {
+           // alert("加载失败");
+        }
+    });
+
+    return false;
+}
---
+++ waterfall/Scripts/common.js
@@ -0,0 +1,138 @@
+﻿function cb_CodeHighlight() {
+    SyntaxHighlighter.config.strings.viewSource = "view my source!!!!";
+    SyntaxHighlighter.highlight();
+
+    psComments();
+}
+
+/*美化评论*/
+function psComments() {
+    var w1 = '<div class="list">' +
+        '<table class="out" border="0" cellspacing="0" cellpadding="0"> ' +
+			'<tr>' +
+				'<td class="icontd" align="left" valign="bottom">' +
+                    '<img src="http://pic.cnblogs.com/avatar/simple_avatar.gif" width="39px" height="39px"/>'
+    '</td>' +
+				'<td align="left" valign="bottom" class="q">' +
+					'<table border="0" cellpadding="0" cellspacing="0" style=""> ' +
+						'<tr><td class="topleft"></td><td class="top"></td><td class="topright"></td></tr> ' +
+						'<tr><td class="left"></td> <td align="left" class="conmts"><p>';
+
+
+    var w2 = '</p> </td> <td class="right"></td></tr> ' +
+						'<tr><td class="bottomleft"></td><td class="bottom"></td><td class="bottomright"></td></tr> ' +
+					'</table>' +
+				'</td> ' +
+			'</tr> ' +
+		'</table> ' +
+    '</div>';
+
+
+    var louzhu1 = '<div class="list">' +
+        '<table class="inc" border="0" cellspacing="0" cellpadding="0"> ' +
+			'<tr>' +
+				'<td align="right" valign="bottom" class="q">' +
+					'<table border="0" cellpadding="0" cellspacing="0" style=""> ' +
+						'<tr><td class="topleft"></td><td class="top"></td><td class="topright"></td></tr> ' +
+						'<tr><td class="left"></td> <td align="left" class="conmts"><p>';
+
+
+    var louzhu2 = '</p> </td> <td class="right"></td></tr> ' +
+						'<tr><td class="bottomleft"></td><td class="bottom"></td><td class="bottomright"></td></tr> ' +
+					'</table>' +
+				'</td> ' +
+				'<td class="icontd" align="left" valign="bottom">' +
+                    '<img src="http://pic.cnblogs.com/avatar/simple_avatar.gif" width="39px" height="39px"/>' +
+				'</td>' +
+			'</tr> ' +
+		'</table> ' +
+    '</div>';
+
+    $.each($(".feedbackItem"), function (i, feedbackItem) {
+        var fbi = $(feedbackItem);
+        var lz = fbi.find(".louzhu");
+
+        var avatar = "";
+        $.each(fbi.find("a"), function (i, link) {
+            var href = $(link).attr("href");
+            if (/\/u\/(\d+)/.test(href)) {
+                avatar = p + "a" + RegExp.$1 + ".jpg";
+            }
+        });
+
+        var comment_body = fbi.find(".blog_comment_body");
+        var html = "";
+
+        if (lz.length > 0) {
+            html = w1 + comment_body.html() + w2;
+        } else {
+            html = louzhu1 + comment_body.html() + louzhu2;
+        }
+
+        if (avatar) {
+            html.replace("http://pic.cnblogs.com/avatar/simple_avatar.gif", avatar)
+        }
+
+        comment_body.html(html);
+
+    });
+}
+
+function psComments() {
+    var w1 = '<div class="list">' +
+        '<table class="out" border="0" cellspacing="0" cellpadding="0"> ' +
+			'<tr>' +
+				'<td align="left" valign="bottom" class="q">' +
+					'<table border="0" cellpadding="0" cellspacing="0" style=""> ' +
+						'<tr><td class="topleft"></td><td class="top"></td><td class="topright"></td></tr> ' +
+						'<tr><td class="left"></td> <td align="left" class="conmts"><p>';
+
+
+    var w2 = '</p> </td> <td class="right"></td></tr> ' +
+						'<tr><td class="bottomleft"></td><td class="bottom"></td><td class="bottomright"></td></tr> ' +
+					'</table>' +
+				'</td> ' +
+				'<td class="icontd" align="left" valign="bottom">' +
+                    '<img src="http://pic.cnblogs.com/avatar/simple_avatar.gif" width="39px" height="39px"/>'
+                '</td>' +
+			'</tr> ' +
+		'</table> ' +
+    '</div>';
+
+    $.each($(".blog_comment_body"), function (i, t) {        
+        $(t).html(w1 + $(t).html() + w2);
+    });
+
+    $(".louzhu").parent().find('.out')
+                .removeClass("out").addClass("inc")
+		.find(".q").attr("align", "right");
+
+    /* 20120927 Freewind 同学提供的获取头像技巧*/
+    var p = "http://pic.cnblogs.com/avatar/";
+    var c = document.getElementById("blog-comments-placeholder");
+    var d = c.childNodes; 
+    for (i = 0; i < d.length; i++) {
+        if (d[i].className == "feedbackItem") {
+            var t = d[i].getElementsByTagName("table")[0];
+            var t0 = t.rows[0];
+            t0.cells[1].style.display = "block";
+            if (/\/u\/(\d+)/.test(d[i].innerHTML)) {
+                img = t0.cells[1].getElementsByTagName("img")[0];
+                img.src = p + "a" + RegExp.$1 + ".jpg";
+                img.onerror = function () {
+                    this.src = p + 'simple_avatar.gif';
+                };
+            }
+
+            if (t.className == "out") {
+                t0.appendChild(t0.cells[0]);
+            }
+        }
+    };
+
+}
+
+$(document).ready(function () {
+
+   
+}); 
